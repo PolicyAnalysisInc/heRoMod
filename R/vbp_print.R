03932c57a603b06eb9bdf4a75ba5ac328d9ece71
Fernando Alarid-Escudero <alari006@umn.edu>
2017-10-13 20:12:18 -0500

    Update
    Clean code

--- R/vbp_print.R
+++ R/vbp_print.R
@@ -15,14 +15,11 @@
 #' 
 plot.vbp <- function(x, 
                      bw = FALSE, ...) {
-  res <- ggplot2::ggplot(x$p_vs_wtp, aes(
-    x = WTP, 
-    y = Price, 
-    color = Comparison)) +
-    ggplot2::geom_line(linetype = 3) +
-    ggplot2::geom_line(data = x$vbp, aes(
-      x = WTP, 
-      y = Price), 
+  res <- ggplot2::ggplot(x$p_vs_wtp, 
+                         aes(x = WTP, y = Price, color = Comparison)) +
+    ggplot2::geom_line(linetype = "dotted") +
+    ggplot2::geom_line(data = x$vbp, 
+                       aes(x = WTP, y = Price), 
       linetype = 1,
       color = "black") +
     ggplot2::xlab("WTP Threshold")
@@ -37,7 +34,7 @@ plot.vbp <- function(x,
 }
 
 #' @export
-print.summary_dsa <- function(x, ...) {
+print.summary_vbp <- function(x, ...) {
   v <- x$object$variables
   cat(sprintf("A sensitivity analysis on %i parameters.\n\n",
               length(v)))
@@ -63,68 +60,11 @@ print.summary_dsa <- function(x, ...) {
 }
 
 #' @export
-print.dsa <- function(x, ...) {
+print.vbp <- function(x, ...) {
   print(summary(x))
 }
 
-get_central_strategy.dsa <- function(x, ...) {
+get_central_strategy.vbp <- function(x, ...) {
   get_central_strategy(get_model(x))
 }
 
-#' @rdname heemod_scale
-scale.dsa <- function(x, center = TRUE, scale = TRUE) {
-  .bm <- get_central_strategy(x)
-  
-  res <- x$dsa
-  
-  if (scale) {
-    res <- res %>% 
-      dplyr::mutate_(
-        .cost = ~ .cost / .n_indiv,
-        .effect = ~ .effect / .n_indiv
-      )
-  }
-  
-  if (center) {
-    res <- res %>% 
-      dplyr::group_by_(~ .par_names, ~ .par_value) %>% 
-      dplyr::mutate_(
-        .cost = ~ .cost - sum(.cost * (.strategy_names == .bm)),
-        .effect = ~ .effect - sum(.effect * (.strategy_names == .bm))
-      ) %>% 
-      dplyr::ungroup()
-  }
-  res
-}
-
-#' @export
-summary.dsa <- function(object, ...) {
-  res <- object %>% 
-    scale(...) %>% 
-    dplyr::group_by_(~ .par_names, ~ .par_value)  %>% 
-    dplyr::do_(~ compute_icer(
-      ., strategy_order = order(get_effect(get_model(object)))
-    )) %>% 
-    dplyr::ungroup()
-  
-  structure(
-    list(
-      res_comp = res,
-      object = object
-    ),
-    class = c("summary_dsa", class(res))
-  )
-}
-
-digits_at_diff <- function(x, y, addl_digits = 1){
-  stopifnot(length(x) == length(y))
-  diff <- abs(x - y)
-  num_digits <- -floor(log(diff, 10)) + addl_digits
-  round_x <- 
-    sapply(seq(along = x), 
-           function(i){round(x[i], num_digits[i])})
-  round_y <- 
-    sapply(seq(along = y), 
-           function(i){round(y[i], num_digits[i])})
-  list(x = round_x, y = round_y, nd = num_digits)
-}
