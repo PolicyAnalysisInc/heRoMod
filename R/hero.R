98137dca917f88960a319d773fd8a3321b39dd34
Jordan Amdahl <jrdnmdhl@gmail.com>
2019-02-04 13:02:20 -0500

    Fix casing

--- R/hero.R
+++ R/hero.R
@@ -1235,7 +1235,7 @@ run_hero_model <- function(...) {
         )) %>%
         purrr::keep(function(x) {
           isNull <- is.null(x)
-          dimensions <- c(nRow(.), nCol(.))
+          dimensions <- c(nrow(.), ncol(.))
           !isNull && !all(is.na(dimensions)) && all(dimensions) > 0
         })
       writeWorkbook(lapply(wb_list, as.data.frame), "model.xlsx")
@@ -1821,7 +1821,7 @@ export_hero_xlsx <- function(...) {
     )) %>%
     purrr::keep(function(x) {
       isNull <- is.null(x)
-      dimensions <- c(nRow(.), nCol(.))
+      dimensions <- c(nrow(.), ncol(.))
       !isNull && !all(is.na(dimensions)) && all(dimensions) > 0
     })
   writeWorkbook(lapply(wb_list, as.data.frame), paste0(dots$name, ".xlsx"))
