e263b16809c0922a88603996a8e35bdbd5453138
Jordan Amdahl <jrdnmdhl@gmail.com>
2019-02-10 01:20:32 -0500

    Additions to testing

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Travis build status](https://travis-ci.org/PolicyAnalysisInc/heRoMod.svg?branch=master)](https://travis-ci.org/PolicyAnalysisInc/heRoMod)
+ [![Coverage status](https://codecov.io/gh/PolicyAnalysisInc/heRoMod/branch/master/graph/badge.svg)](https://codecov.io/github/PolicyAnalysisInc/heRoMod?branch=master)
 
 # heRomod - A Package for Health Economic Modeling
 
--- tests/testthat/test_hero.R
+++ tests/testthat/test_hero.R
@@ -1,15 +1,12 @@
 context("Running heRo model")
 
-
-
-
 test_that(
   "Example Simple PSM Runs Correctly", {
-    example_simple_psm <- readRDS(system.file("hero","example_simple_psm", "model.rds", package="heRomod"))
+    model <- readRDS(system.file("hero","example_simple_psm", "model.rds", package="heRomod"))
     
     # Base Case Results
     bc_res <- readRDS(system.file("hero","example_simple_psm", "bc_res.rds", package="heRomod"))
-    bc_res_test <- do.call(run_hero_bc, example_simple_psm)
+    bc_res_test <- do.call(run_hero_bc, model)
     
     expect_equal(
       bc_res$trace,
@@ -36,12 +33,285 @@ test_that(
       bc_res_test$ce
     )
     
-    vbp_res <- do.call(run_hero_vbp,example_simple_psm)
-    dsa_res <- do.call(run_hero_dsa,example_simple_psm)
-    psa_res <- do.call(run_hero_psa,example_simple_psm)
+    vbp_res <- readRDS(system.file("hero","example_simple_psm", "vbp_res.rds", package="heRomod"))
+    vbp_res_test <- do.call(run_hero_vbp,model)
+    
+    
+    expect_equal(
+      vbp_res$eq,
+      vbp_res_test$eq
+    )
+    
+    dsa_res <- readRDS(system.file("hero","example_simple_psm", "dsa_res.rds", package="heRomod"))
+    dsa_res_test <- do.call(run_hero_dsa,model)
+    
+    expect_equal(
+      dsa_res$main,
+      dsa_res_test$main
+    )
+    
+    expect_equal(
+      dsa_res$outcomes,
+      dsa_res_test$outcomes
+    )
+    
+    expect_equal(
+      dsa_res$cost,
+      dsa_res_test$cost
+    )
+    
+    expect_equal(
+      dsa_res$ce,
+      dsa_res_test$ce
+    )
+    
+    expect_equal(
+      dsa_res$nmb,
+      dsa_res_test$nmb
+    )
+    
+    psa_res <- do.call(run_hero_psa,model)
     withr::with_dir(new = tempdir(), {
-      example_simple_psm$name <- 'test.xlsx'
-      do.call(export_hero_xlsx,example_simple_psm)
+      model$name <- 'test'
+      do.call(export_hero_xlsx,model)
+      xl_file <- openxlsx::read.xlsx('test.xlsx')
     })
     
 })
+
+test_that(
+  "PSM Responders Model Runs Correctly", {
+    model <- readRDS(system.file("hero","psm_responders", "model.rds", package="heRomod"))
+    
+    # Base Case Results
+    bc_res <- readRDS(system.file("hero","psm_responders", "bc_res.rds", package="heRomod"))
+    bc_res_test <- do.call(run_hero_bc, model)
+    
+    expect_equal(
+      bc_res$trace,
+      bc_res_test$trace
+    )
+    
+    expect_equal(
+      bc_res$outcomes,
+      bc_res_test$outcomes
+    )
+    
+    expect_equal(
+      bc_res$costs,
+      bc_res_test$costs
+    )
+    
+    expect_equal(
+      bc_res$nmb,
+      bc_res_test$nmb
+    )
+    
+    expect_equal(
+      bc_res$ce,
+      bc_res_test$ce
+    )
+    
+    vbp_res <- readRDS(system.file("hero","example_simple_psm", "vbp_res.rds", package="heRomod"))
+    vbp_res_test <- do.call(run_hero_vbp,model)
+    
+    
+    expect_equal(
+      vbp_res$eq,
+      vbp_res_test$eq
+    )
+    
+    dsa_res <- readRDS(system.file("hero","example_simple_psm", "dsa_res.rds", package="heRomod"))
+    dsa_res_test <- do.call(run_hero_dsa,model)
+    
+    expect_equal(
+      dsa_res$main,
+      dsa_res_test$main
+    )
+    
+    expect_equal(
+      dsa_res$outcomes,
+      dsa_res_test$outcomes
+    )
+    
+    expect_equal(
+      dsa_res$cost,
+      dsa_res_test$cost
+    )
+    
+    expect_equal(
+      dsa_res$ce,
+      dsa_res_test$ce
+    )
+    
+    expect_equal(
+      dsa_res$nmb,
+      dsa_res_test$nmb
+    )
+    
+    psa_res <- do.call(run_hero_psa,model)
+    withr::with_dir(new = tempdir(), {
+      model$name <- 'test'
+      do.call(export_hero_xlsx,model)
+      xl_file <- openxlsx::read.xlsx('test.xlsx')
+    })
+    
+  })
+
+test_that(
+  "Groups Model Runs Correctly", {
+    model <- readRDS(system.file("hero","groups", "model.rds", package="heRomod"))
+    
+    # Base Case Results
+    bc_res <- readRDS(system.file("hero","groups", "bc_res.rds", package="heRomod"))
+    bc_res_test <- do.call(run_hero_bc, model)
+    
+    expect_equal(
+      bc_res$trace,
+      bc_res_test$trace
+    )
+    
+    expect_equal(
+      bc_res$outcomes,
+      bc_res_test$outcomes
+    )
+    
+    expect_equal(
+      bc_res$costs,
+      bc_res_test$costs
+    )
+    
+    expect_equal(
+      bc_res$nmb,
+      bc_res_test$nmb
+    )
+    
+    expect_equal(
+      bc_res$ce,
+      bc_res_test$ce
+    )
+    
+    vbp_res <- readRDS(system.file("hero","groups", "vbp_res.rds", package="heRomod"))
+    vbp_res_test <- do.call(run_hero_vbp,model)
+    
+    
+    expect_equal(
+      vbp_res$eq,
+      vbp_res_test$eq
+    )
+    
+    dsa_res <- readRDS(system.file("hero","groups", "dsa_res.rds", package="heRomod"))
+    dsa_res_test <- do.call(run_hero_dsa,model)
+    
+    expect_equal(
+      dsa_res$main,
+      dsa_res_test$main
+    )
+    
+    expect_equal(
+      dsa_res$outcomes,
+      dsa_res_test$outcomes
+    )
+    
+    expect_equal(
+      dsa_res$cost,
+      dsa_res_test$cost
+    )
+    
+    expect_equal(
+      dsa_res$ce,
+      dsa_res_test$ce
+    )
+    
+    expect_equal(
+      dsa_res$nmb,
+      dsa_res_test$nmb
+    )
+
+    withr::with_dir(new = tempdir(), {
+      model$name <- 'test'
+      do.call(export_hero_xlsx,model)
+      xl_file <- openxlsx::read.xlsx('test.xlsx')
+    })
+    
+  })
+
+test_that(
+  "Markov model Runs Correctly", {
+    model <- readRDS(system.file("hero","markov_model", "model.rds", package="heRomod"))
+    
+    # Base Case Results
+    bc_res <- readRDS(system.file("hero","markov_model", "bc_res.rds", package="heRomod"))
+    bc_res_test <- do.call(run_hero_bc, model)
+    
+    expect_equal(
+      bc_res$trace,
+      bc_res_test$trace
+    )
+    
+    expect_equal(
+      bc_res$outcomes,
+      bc_res_test$outcomes
+    )
+    
+    expect_equal(
+      bc_res$costs,
+      bc_res_test$costs
+    )
+    
+    expect_equal(
+      bc_res$nmb,
+      bc_res_test$nmb
+    )
+    
+    expect_equal(
+      bc_res$ce,
+      bc_res_test$ce
+    )
+    
+    vbp_res <- readRDS(system.file("hero","markov_model", "vbp_res.rds", package="heRomod"))
+    vbp_res_test <- do.call(run_hero_vbp,model)
+    
+    
+    expect_equal(
+      vbp_res$eq,
+      vbp_res_test$eq
+    )
+    
+    dsa_res <- readRDS(system.file("hero","markov_model", "dsa_res.rds", package="heRomod"))
+    dsa_res_test <- do.call(run_hero_dsa,model)
+    
+    expect_equal(
+      dsa_res$main,
+      dsa_res_test$main
+    )
+    
+    expect_equal(
+      dsa_res$outcomes,
+      dsa_res_test$outcomes
+    )
+    
+    expect_equal(
+      dsa_res$cost,
+      dsa_res_test$cost
+    )
+    
+    expect_equal(
+      dsa_res$ce,
+      dsa_res_test$ce
+    )
+    
+    expect_equal(
+      dsa_res$nmb,
+      dsa_res_test$nmb
+    )
+    
+    psa_res <- do.call(run_hero_psa,model)
+    withr::with_dir(new = tempdir(), {
+      model$name <- 'test'
+      do.call(export_hero_xlsx,model)
+      xl_file <- openxlsx::read.xlsx('test.xlsx')
+    })
+    
+  })
+
