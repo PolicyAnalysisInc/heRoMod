9b84631641e41e73d725abe8b2f8838f9d8725d0
jrdnmdhl <jrdnmdhl@gmail.com>
2021-08-25 15:09:12 -0400

    test

--- R/hero_scenario.R
+++ R/hero_scenario.R
@@ -44,7 +44,6 @@ run_hero_scen <- function(...) {
   
   # Pull out results for each scenario
   outcomes_res <- extract_sa_summary_res(res, dots$hsumms, c('.scenario'))
-  stop('foo')
   costs_res <- extract_sa_summary_res(res, dots$esumms, c('.scenario'))
   nmb_res <- extract_sa_nmb(outcomes_res, costs_res, dots$hsumms, dots$esumms, c('.scenario'))
   
